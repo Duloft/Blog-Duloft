6c798c605c85f33f3a3698dbe66ad7ed72cf168e
Great-special <sirgreatspecial@gmail.com>
2023-09-01 15:55:25 +0100

    all set

--- Blog_DuLoft/settings.py
+++ Blog_DuLoft/settings.py
@@ -83,7 +83,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    
+    # 'default':{
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'railway',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'ZTJ7B1tNgJ70IqMbMPLu',
+    #     'HOST': 'containers-us-west-107.railway.app',
+    #     'PORT': '7919',
+    # }
+    
 }
 
 
--- waitlist/forms.py
+++ waitlist/forms.py
@@ -3,9 +3,20 @@ from .models import Waitlist
 
 
 class WaitListForm(forms.ModelForm):
+    own_a_house = forms.BooleanField(label="I own a house and I want to rent or sell", 
+                                     widget=forms.CheckboxInput(attrs={'class':'form-control-check-input'}))
     
+    manage_my_property = forms.BooleanField(label="I want DuLoft to manage my property", 
+                                     widget=forms.CheckboxInput(attrs={'class':'form-control-check-input'}))
     class Meta:
         model = Waitlist
         fields = "__all__"
         
+        widgets = {
+            'full_name': forms.TextInput(attrs={'class':'form-control'}),
+            'phone_number': forms.NumberInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),  
+            'state': forms.TextInput(attrs={'class': 'form-control'}),
+            'location': forms.TextInput(attrs={'class':'form-control', 'placeholder':'The area in your state where you live.'})
+        }
         
