86a77dbe23cd0c315dba27327c49b3f5741f8ac2
Great-special <sirgreatspecial@gmail.com>
2023-09-14 12:38:23 +0100

    final test deployment

--- Blog_DuLoft/settings.py
+++ Blog_DuLoft/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['duloft.com', '127.0.0.1', 'app.duloft.com']
 ALLOWED_HOSTS = ['*']
