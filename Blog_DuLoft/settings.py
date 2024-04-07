f198280cc2ea99448c2356c15f50d4d1b3a66172
Great-special <sirgreatspecial@gmail.com>
2024-04-07 02:29:17 +0100

    SOS update...

--- Blog_DuLoft/settings.py
+++ Blog_DuLoft/settings.py
@@ -181,7 +181,7 @@ STORAGES = {
 }
 
 # Django variable. URL for unlogged users. We redirect it to our view.
-LOGIN_URL = '/sos/login/'
+LOGIN_URL = '/user/login/'
 
 SSO = {
     # Specify SSO server base url (REQUIRED)
--- accounts/views.py
+++ accounts/views.py
@@ -7,12 +7,12 @@ from django.conf import settings
 
 def login(request):
     # Construct the login URL for the SSO server
-    login_url = f"{settings.SOS['ROOT']}/sso/login/?next={reverse('blog_post')}"
+    login_url = f"{settings.SSO['ROOT']}/sso/login/?next={reverse('blog_post')}"
 
     # Redirect the user to the SSO server for authentication
     return redirect(login_url)
 
 
 def logout(request):
-    sso_logout_url = f"{settings.SSO['ROOT']}/sos/logout/"
+    sso_logout_url = f"{settings.SSO['ROOT']}/sso/logout/"
     return redirect(sso_logout_url)
